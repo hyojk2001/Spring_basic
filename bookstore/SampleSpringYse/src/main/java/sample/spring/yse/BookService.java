5e426bce5368c71c82dca3257b524ed82faaef8b
hyojk2001 <32731442+hyojk2001@users.noreply.github.com>
2021-05-06 17:18:08 +0900

    Add files via upload

---
+++ bookstore/SampleSpringYse/src/main/java/sample/spring/yse/BookService.java
@@ -0,0 +1,13 @@
+package sample.spring.yse;
+
+import java.util.List;
+import java.util.Map;
+
+public interface BookService {
+	
+	Map<String, Object> detail(Map<String, Object> map);
+	String create(Map<String, Object> map);
+	boolean edit(Map<String, Object> map);  
+	boolean remove(Map<String, Object> map);  
+	List<Map<String, Object>> list(Map<String, Object> map);  
+}
